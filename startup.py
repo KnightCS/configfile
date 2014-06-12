7e94142cfa39d1a34c26a179a9f6a071c818c2bb
KnightCS <chenshuomailbox@gmail.com>
2014-06-12 13:45:59 +0800

    change the python start file

--- startup.py
+++ startup.py
@@ -1,10 +1,3 @@
-#*****~*****~*****~*****~*****~*****~*****~*****~*****
-# >File Name:    .pythonstartup.py
-# >Author:       Knight_cs (chenshuo_mailbox@gmail.com)
-# >Created Time: 2014年06月02日 星期一 23时29分15秒
-# >Program:python启动脚本
-# 2014_06_02:增加python shell的tab功能
-#*****~*****~*****~*****~*****~*****~*****~*****~*****
 import sys
 import readline
 import rlcompleter
@@ -19,6 +12,5 @@ try:
 except IOError:
     pass
 atexit.register(readline.write_history_file, histfile)
-
 
 del os, histfile, readline, rlcompleter
