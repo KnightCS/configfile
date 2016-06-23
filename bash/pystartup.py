13b067cd1064e52289ccf616ffa2ddacb8c1d5fc
Knight_CS <chenshuomailbox@gmail.com>
2016-06-24 00:04:20 +0800

    去掉没有使用的import

--- bash/pystartup.py
+++ bash/pystartup.py
@@ -1,10 +1,20 @@
-import sys
+#!/usr/bin/python
+#-*- coding: UTF-8 -*-
+# >FileName    : pystartup.py
+# >Author      : KnightCS
+# >Description :
+# > version 1.0: 2016-06-23 23:47:26
+#   用于初始化python
+#       1. python命令行tab自动补全
+#       2. 记录python命令行历史
+
 import readline
-import rlcompleter
 import atexit
 import os
+
 # tab completion
 readline.parse_and_bind('tab: complete')
+
 # history file
 histfile = os.path.join(os.environ['HOME'], '.pythonhistory')
 try:
@@ -13,4 +23,4 @@ except IOError:
     pass
 atexit.register(readline.write_history_file, histfile)
 
-del os, histfile, readline, rlcompleter
+del os, histfile, readline
